cf70a013e62a00513f7f2deb9fa41d132002bac3
Aditi-Writes <aditideoghare23@gmail.com>
2025-02-07 15:42:12 +0530

    nano script.py

--- nano script.py
+++ nano script.py
@@ -5,14 +5,12 @@ df = pd.read_csv("dirty_data.csv")
 
 print("Original Data:\n", df)
 
-# 3️⃣ Handle missing values
 df['Age'].fillna(df['Age'].median(), inplace=True) 
 df['Salary'].fillna(df['Salary'].mean(), inplace=True)
 
 df.drop_duplicates(inplace=True)
 
 df['Joining_Date'] = pd.to_datetime(df['Joining_Date'])
-
 df['Department'] = df['Department'].str.lower()
 
 df.to_csv("cleaned_data.csv", index=False)
